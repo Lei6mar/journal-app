5f858fb859d30a1f707a8b3d869183eaeab511ad
miledesmam <miledesmam@gmail.com>
2021-10-01 01:07:16 -0500

    wip

---
+++ src/components/notes/NoteScreen.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { NotesAppbar } from './NotesAppbar'
+
+export const NoteScreen = () => {
+  return (
+    <div className="notes__main-content">
+      <NotesAppbar />
+    </div>
+  )
+}
---
+++ src/components/notes/NotesAppbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const NotesAppbar = () => {
+  return (
+    <div className="notes__appbar">
+      <span>28 de agosto 2020</span>
+      <div>
+        <button className="btn">
+          Picture
+        </button>
+      </div>
+    </div>
+  )
+}
