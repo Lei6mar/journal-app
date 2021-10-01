01e2153fd0ad89ce7ba31b9e25560fe05941dd1c
miledesmam <miledesmam@gmail.com>
2021-10-01 01:23:44 -0500

    styles finished

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -5,6 +5,15 @@ export const NoteScreen = () => {
   return (
     <div className="notes__main-content">
       <NotesAppbar />
+      <div className="notes__content">
+        <input type="text" placeholder="some awesome title" className="notes__title-input" autoComplete="off"/>
+        <textarea
+          placeholder="what happened today"
+          className="notes__textarea"></textarea>
+          <div className="notes__image">
+            <img src="https://iso.500px.com/wp-content/uploads/2014/07/big-one.jpg" alt="Imagen"/>
+          </div>
+      </div>
     </div>
   )
 }
