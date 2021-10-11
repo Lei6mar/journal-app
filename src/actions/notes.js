1a640c5c128127a6a6923d6ad4eb5b3faff7c4a6
miledesmam <miledesmam@gmail.com>
2021-10-10 20:20:00 -0500

    WIP crud

--- src/actions/notes.js
+++ src/actions/notes.js
@@ -1,30 +1,37 @@
-import { addDoc, collection, firestore } from "../firebase/firebase-init"
-import { types } from "../types/types"
+import { addDoc, collection, firestore } from "../firebase/firebase-init";
+import { loadNotes } from "../helpers/loadNotes";
+import { types } from "../types/types";
 
-export const startNewNote =  () => {
+export const startNewNote = () => {
   return async (dispatch, getState) => {
-    const {uid} = getState().auth
+    const { uid } = getState().auth;
     const newNote = {
-      title: '',
-      body: '',
-      date: new Date().getTime()
-    }
-    const collectionRef = collection(firestore,`${uid}/journal/notes`);
-    const doc = await addDoc(collectionRef, newNote)
-    dispatch(activeNote(doc.id, newNote))
-  }
-}
+      title: "",
+      body: "",
+      date: new Date().getTime(),
+    };
+    const collectionRef = collection(firestore, `${uid}/journal/notes`);
+    const doc = await addDoc(collectionRef, newNote);
+    dispatch(activeNote(doc.id, newNote));
+  };
+};
 
 export const activeNote = (id, note) => ({
   type: types.notesActive,
   payload: {
     id,
-    ...note
-  }
-})
+    ...note,
+  },
+});
+
+export const startLoadingNotes = (uid) => {
+  return async (dispatch) => {
+    const notes = await loadNotes(uid);
+    dispatch(setNotes(notes));
+  };
+};
 
 export const setNotes = (notes) => ({
   type: types.notesLoad,
-  payload: notes
-})
-
+  payload: notes,
+});
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -7,8 +7,7 @@ import { auth, onAuthStateChanged } from "../firebase/firebase-init";
 import { login } from "../actions/auth";
 import { PrivateRoute } from "./PrivateRoute";
 import { PublicRoute } from "./PublicRoute";
-import { loadNotes } from "../helpers/loadNotes";
-import { setNotes } from "../actions/notes";
+import { startLoadingNotes } from "../actions/notes";
 
 export const AppRouter = () => {
 
@@ -20,12 +19,11 @@ export const AppRouter = () => {
 
   useEffect(() => {
 
-    onAuthStateChanged(auth, async (user)=> {
+    onAuthStateChanged(auth, (user)=> {
       if(user?.uid){
         dispatch(login(user.uid, user.displayName));
         setIsLoggedIn(true);
-        const notes = await loadNotes(user.uid)
-        dispatch(setNotes(notes))
+        dispatch(startLoadingNotes(user.uid))
       }else{
         setIsLoggedIn(false);
       }
